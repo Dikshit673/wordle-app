f59ce460050ec557f9ec1a5eb0a75509e901dfe1
dikshit673 <sachinnavodian@gmail.com>
2025-07-06 20:02:48 +0530

    updates layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <div>getting started with react and typescript</div>;
+  return (
+    <section className='min-h-screen w-full bg-gray-300'>
+      <div className='mx-auto w-9/10 md:w-7/10 lg:w-5/10 xl:w-3/10 2xl:w-4/10'>
+        <div className='flex min-h-screen flex-col items-center justify-center bg-white p-4'>
+          <h1 className='mb-4 text-3xl font-bold'>Wordle</h1>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
